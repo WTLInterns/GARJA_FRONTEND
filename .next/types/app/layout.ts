63c5cfae42b96bed84f8b9ba10905d15ea3ed449
jaywantmhala <jaywant61495@gmail.com>
2025-09-13 18:41:08 +0530

    sdfdsf

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\WTL_Abhishek\React\GARJA_FRONTEND\src\app\layout.tsx
+// File: C:\Users\jaywa\Desktop\Abacus\garja\GARJA_FRONTEND\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
